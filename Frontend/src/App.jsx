dcd04239767ce22a6b04c5c103b35bfe86528825
liNikham <mahadiknikhil2508@gmail.com>
2024-10-05 19:35:03 +0530

    add signup page ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Button } from "flowbite-react"
 import TodoList from "./components/TodoList"
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import SignUp from "./pages/SignUp"
 export default function App() {
   return (
-    <div>
-      <TodoList />
-    </div>
-    
+     <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<TodoList />} /> 
+        <Route path="/signup" element={<SignUp />} />
+      </Routes> 
+     </BrowserRouter> 
   )
 }
